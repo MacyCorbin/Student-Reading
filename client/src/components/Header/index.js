916c6a677cc43c77b5b28cc7a0efa3fe4cfffb02
Macy Corbin <macycorbin@Macys-MacBook-Pro.local>
2019-08-09 22:05:52 -0400

    updated css

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -4,7 +4,7 @@ import "./Header.css"
 function Header() {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-white" style = {{ margin: "10px 30px"}}>
-      <a className="navbar-brand headerFont" href="/">
+      <a className="navbar-brand headerFont"style={{color:"#33658A"}} href="/">
         BookBuster
       </a>
       <img src="books.png" alt="books graphic" width= "50px"></img>
--- client/src/components/MainNav/index.js
+++ client/src/components/MainNav/index.js
@@ -3,26 +3,22 @@ import "./MainNav.css"
 
 function MainNav(props) {
   return (
-    <nav className="navbar navbar-dark bg" style={{ paddingRight: "0px" }}>
+    <nav className="navbar navbar-dark bg" style = {{ paddingRight: "0px"}}>
 
-
-        
-        <a className="navbar" href="/WhatWeDo" onClick={() => props.navBarSelection(1)}>
+    <span>
+      <a className="navbar" href="/WhatWeDo" onClick={()=>props.navBarSelection(1)}>
         WHAT WE DO
-       </a>	      
+      </a>
+    </span>
 
-
-
-
-      <span className="navbar navbar-right">
-        <a className="navbar-brand" href="/teacherregister">
-        TEACHER REGISTER
-        </a>     
-
-        <a className="navbar" href="/studentregister">
-        STUDENT REGISTER
-        </a>
-      </span>	   
+    <span className="navbar" >
+      <a className="navbar"  href="/teacherregister">
+          TEACHER REGISTER
+      </a> 
+      <a className="navbar" href="/studentregister">
+          STUDENT REGISTER
+      </a>   
+    </span> 
 
 
     </nav>
